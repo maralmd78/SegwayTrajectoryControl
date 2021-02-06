71d48b4f566c25eacfce81a017997178a4536e21
Maral Mordad <maralmordad78@gmail.com>
2021-02-06 23:09:55 +0330

    state feedback controller for xdot

--- segwayModel.m
+++ segwayModel.m
@@ -34,36 +34,60 @@ B = [0;
  tf_x_input = tf(num,den);
  
  
- %% controller - theoric
+ %% theta controller - theoric 
 Gc_theta = pid(834,1112,139);
 sys_theta = feedback(Gc_theta * tf_theta_input,1);
 
 figure(1);
 subplot(2,2,1);
 rlocus(tf_theta_input);
-title("root locus - without controller");
+title("root locus theta - without controller");
 subplot(2,2,2);
 rlocus(tf_theta_input*Gc_theta);
-title("root locus - with controller");
+title("root locus theta - with controller");
 subplot(2,2,3);
 step(tf_theta_input);
-title("step response - open loop");
+title("step response theta - open loop");
 subplot(2,2,4);
 step(sys_theta,10);
-title("step response - with controller");
+title("step response theta - with controller");
 stepinfo(sys_theta)
 
-%% controller - tune
+%% theta controller - tune
 Gc_theta_tune = pid(930,1650,300);
 sys_theta_tune = feedback(Gc_theta_tune * tf_theta_input,1);
 figure(2);
 subplot(2,1,1);
 step(sys_theta,3);
-title("step response - theoric");
+title("step response theta - theoric");
 subplot(2,1,2);
 step(sys_theta_tune,3);
-title("step response - tuned");
+title("step response theta - tuned");
 stepinfo(sys_theta_tune)
+
+%% speed(xdot) controller - theoric
+% state feedback pole placement
+K = acker(A,B,[-1+i -1-i 0 -15]);
+Anew = A - B*K;
+[num,den] = ss2tf(Anew,B,C(1,1:end),0);
+tf_xdot_input_new = tf(num,den);
+sys_xdot = tf_xdot_input_new/dcgain(tf_xdot_input_new);
+
+figure(3);
+subplot(2,2,1);
+rlocus(tf_xdot_input);
+title("root locus xdot - without controller");
+subplot(2,2,2);
+rlocus(sys_xdot);
+title("root locus xdot - with controller");
+subplot(2,2,3);
+step(tf_xdot_input);
+title("step response xdot - open loop");
+subplot(2,2,4);
+step(sys_xdot,10);
+title("step response xdot - with controller");
+stepinfo(sys_xdot)
+
 
 
  
